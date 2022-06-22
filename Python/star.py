228454d5fd4a3e31f78eaceb51bccc262479e264
melony <hjz1010@gmail.com>
2022-06-22 09:58:39 +0900

    터미널에서 실행할 수 있게 경로 설정

--- Python/star.py
+++ Python/star.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # 별찍기 - 크리스마스 트리 - Python
 '''
 print(' ' * (n-1) + '*' * 1 )
@@ -7,6 +9,7 @@ print(' ' * (n-4) + '*' * 7 )
 print(' ' * (n-5) + '*' * 9 )
 '''
 
+
 def tree():
     n = int(input("원하는 트리의 사이즈를 입력하세요: "))
     for i in range(1, n+1):
@@ -15,13 +18,13 @@ def tree():
 
 def tree2():
     n = int(input("원하는 트리의 사이즈를 입력하세요: "))
-    for i in range(1,n+1):
+    for i in range(1, n+1):
         star = '*' * (2*i-1)
         print(star.center(2*n-1))
 
-if __name__== "__main__":
+
+if __name__ == "__main__":
     tree2()
 
 
-#https://www.delftstack.com/ko/howto/python/python-pad-string-with-spaces/
-
+# https://www.delftstack.com/ko/howto/python/python-pad-string-with-spaces/
