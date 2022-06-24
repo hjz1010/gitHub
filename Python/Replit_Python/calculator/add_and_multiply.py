80ac6991b894a7cc1716a76c75a3ef72739d7cf8
melony <hjz1010@gmail.com>
2022-06-24 21:35:20 +0900

    왜 안될까

--- Python/Replit_Python/calculator/add_and_multiply.py
+++ Python/Replit_Python/calculator/add_and_multiply.py
@@ -1,8 +1,9 @@
 
-# from .multiplication import multiply
-from calculator.multiplication import multiply
-#
+from .multiplication import multiply
+# from calculator.multiplication import multiply
+
 
 
 def add_and_multiply(a, b):
     return multiply(a, b) + (a+b)
+
--- Python/Replit_Python/main.py
+++ Python/Replit_Python/main.py
@@ -4,4 +4,3 @@ from calculator.add_and_multiply import add_and_multiply
 
 if __name__ == '__main__':
     print(add_and_multiply(1, 2))
-#
