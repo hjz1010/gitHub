8018ca6d6d2a5d0f416435d96dce2a56c09c3edb
melony <hjz1010@gmail.com>
2022-06-24 20:49:26 +0900

    Replit 57번 패키지 만들기
    main module에서는 상대경로를 사용할 수 없다!

---
+++ Python/Replit_Python/main.py
@@ -0,0 +1,7 @@
+# from .calculator.add_and_multiply import add_and_multiply
+
+from calculator.add_and_multiply import add_and_multiply
+
+if __name__ == '__main__':
+    print(add_and_multiply(1, 2))
+#
