930f5631f9d826cf5552c8c24b6f22a7eef5bc47
melony <hjz1010@gmail.com>
2022-06-25 11:45:20 +0900

    절대경로 에러 해결 시도

--- Python/Replit_Python/calculator/add_and_multiply.py
+++ Python/Replit_Python/calculator/add_and_multiply.py
@@ -1,9 +1,21 @@
 
-from .multiplication import multiply
-# from calculator.multiplication import multiply
+# from os import path
+# import sys
 
+# 상대경로
+# from .multiplication import multiply
+
+# 절대경로
+from multiplication import multiply
 
 
 def add_and_multiply(a, b):
     return multiply(a, b) + (a+b)
 
+
+# print(os.getcwd())  # /Users/hhj/Desktop/gitHub/Python/Replit_Python
+# sys.path.insert(1, path.abspath('.'))
+# sys.path.append(path.abspath('..'))
+# print(path.abspath('.'))
+# print(sys.path)
+# print(__name__)
--- Python/Replit_Python/main.py
+++ Python/Replit_Python/main.py
@@ -1,6 +1,24 @@
+import sys
+from os import path
+
+# 상대경로
 # from .calculator.add_and_multiply import add_and_multiply
 
+# 절대경로
 from calculator.add_and_multiply import add_and_multiply
 
 if __name__ == '__main__':
     print(add_and_multiply(1, 2))
+
+# import os
+# print(os.getcwd())  # /Users/hhj/Desktop/gitHub/Python/Replit_Python
+
+# print(__name__)
+# print(add_and_multiply.__name__)
+
+# sys.path.insert(1, path.abspath('./calculator'))
+# print(path.abspath('./calculator'))
+
+
+sys.path.insert(1, '/Users/hhj/Desktop/gitHub/Python/Replit_Python/calculator')
+print(sys.path)
